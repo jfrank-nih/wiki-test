6fc035334b5237fb0da6e56997816932d631a6d1
James Frank <james.frank@publicissapient.com>
2022-07-22 16:07:37 -0400

    Update commitlint.config.js

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,7 +2,7 @@ module.exports = {
   parserPreset: {
     parserOpts: {
       headerPattern: new RegExp(
-        "^\[#(\d)+\] (.+)$"
+        /^\[#(\d)+\]\s(.+)$/
       ),
       headerCorrespondence: ["references", "subject"],
     }
@@ -12,7 +12,9 @@ module.exports = {
       rules: {
         "header-match-team-pattern": (parsed) => {
           const { references, subject } = parsed;
-          if (references === null && subject === null) {
+          console.log(references);
+          console.log(subject);
+          if (references === null || subject === null) {
             return [
               false,
               "Commit header must be in format '[#issue] subject'",
@@ -24,9 +26,9 @@ module.exports = {
     },
   ],
   rules: {
-    "references-empty": [2, "never"],
+    //"references-empty": [2, "never"],
     "header-match-team-pattern": [2, "always"],
-    "header-min-length": [2, "always", 20],
+    "header-min-length": [2, "always", 21],
     "header-max-length": [2, "always", 72],
   }
 };
