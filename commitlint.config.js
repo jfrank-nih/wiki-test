00e6f3f283ea4bd871fc6685597af197280a342e
James Frank <james.frank@publicissapient.com>
2022-07-22 14:34:04 -0400

    try with no rules

--- commitlint.config.js
+++ commitlint.config.js
@@ -3,8 +3,9 @@ module.exports = {
     "@commitlint/parse"
   ],
   rules: {
-    'references-empty': [2, 'never'],
-    'header-min-length': [2, 'always', 20]
+    //"references-empty": [2, "never"],
+    //"header-min-length": [2, "always", 20],
+    //"header-max-length": [2, "always", 72],
   },
   parserOpts: {
     headerPattern: /^\[#\d+\] .+$/,
