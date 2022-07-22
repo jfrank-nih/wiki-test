0ba66c10ad7bd3337ab48a5668fc5c4c9fc0c91e
James Frank <james.frank@publicissapient.com>
2022-07-22 13:49:21 -0400

    Correct config?

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,10 +1,10 @@
-commitlint: {
-    'rules': {
-      'references-empty': [2, 'never'],
-    },
-    parserPreset: {
-      parserOpts: {
-        issuePrefixes: ['PROJ-']
-      }
-    },
+module.exports = {
+  rules: {
+    'references-empty': [2, 'never'],
+  },
+  parserPreset: {
+    parserOpts: {
+      issuePrefixes: ['PROJ-']
+    }
   }
+};
