1f56220e30753b1e4c867ab6a5c5982b798a5a53
James Frank <james.frank@publicissapient.com>
2022-07-22 14:53:06 -0400

    let's try a custom rule!

--- commitlint.config.js
+++ commitlint.config.js
@@ -5,8 +5,25 @@ module.exports = {
       headerCorrespondence: ["references", "subject"],
    }
   },
+  plugins: [
+    {
+      rules: {
+        "header-match-team-pattern": (parsed) => {
+          const { references, subject } = parsed;
+          if (references === null && subject === null) {
+            return [
+              false,
+              "Commit header must be in format '[#issue] subject'",
+            ];
+          }
+          return [true, ""];
+        }
+      },
+    },
+  ],
   rules: {
     "references-empty": [2, "never"],
+    "header-match-team-pattern": [2, "always"],
     "header-min-length": [2, "always", 20],
     "header-max-length": [2, "always", 72],
   }
